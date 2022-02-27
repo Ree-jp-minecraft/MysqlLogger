1cc638435097181f0ec56629690cbbb7a4078aba
ree-jp <mail@ree-jp.net>
2022-02-28 02:25:00 +0900

    だめなんか...

--- resources/mysql.sql
+++ resources/mysql.sql
@@ -19,6 +19,6 @@ CREATE TABLE IF NOT EXISTS BLOCK_LOG
 -- #    }
 -- #    { send
 -- #    :filePath string
-LOAD DATA LOCAL INFILE ':filePath' INTO TABLE BLOCK_LOG FIELDS TERMINATED BY '*;*';
+LOAD DATA LOCAL INFILE :filePath INTO TABLE BLOCK_LOG FIELDS TERMINATED BY '*;*';
 -- #    }
 -- #}
