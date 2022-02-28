a440e81b8efb0ed6c9bea41e4d35b3dc4200cf82
ree-jp <mail@ree-jp.net>
2022-02-28 14:56:01 +0900

    andつけてねえええ

--- src/ree_jp/mysql_logger/Repository.php
+++ src/ree_jp/mysql_logger/Repository.php
@@ -73,7 +73,7 @@ class Repository
             rename($filePath, $afterPath);
             $this->db->executeImplRaw(["LOAD DATA LOCAL INFILE '$afterPath' INTO TABLE BLOCK_LOG FIELDS TERMINATED BY ';' " .
                 "(@v1, @v2, @v3, @v4, @v5, @v6, @v7, @v8, @v9, @v10) SET action = @v1 AND xuid = @v2 AND x = @v3 AND y = @v4 AND z = @v5 AND " .
-                "world = @v6 AND item = @v7 AND block = @v8 AND server_id = @v9 time = STR_TO_DATE( @v10, '%Y-%m-%d %H:%i:%s');"],
+                "world = @v6 AND item = @v7 AND block = @v8 AND server_id = @v9 AND time = STR_TO_DATE( @v10, '%Y-%m-%d %H:%i:%s');"],
                 [[]], [SqlThread::MODE_GENERIC],
                 function () use ($afterPath): void {
                     unlink($afterPath);
