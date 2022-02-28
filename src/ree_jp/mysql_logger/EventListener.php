415cd0cadf415b1a6c2c674e4eb02c62b9d264b5
Ree-jp <mail@ree-jp.net>
2022-03-01 00:40:28 +0900

    feature bulk insert (#2)

--- src/ree_jp/mysql_logger/EventListener.php
+++ src/ree_jp/mysql_logger/EventListener.php
@@ -5,6 +5,7 @@ namespace ree_jp\mysql_logger;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Listener;
+use ree_jp\mysql_logger\sql\Repository;
 
 class EventListener implements Listener
 {
@@ -18,28 +19,12 @@ class EventListener implements Listener
     public function onBreak(BlockBreakEvent $ev): void
     {
         if ($ev->isCancelled()) return;
-        $this->addBlockLog($ev, "break");
+        $this->repo->addBlockLog($ev, "break");
     }
 
     public function onPlace(BlockPlaceEvent $ev): void
     {
         if ($ev->isCancelled()) return;
-        $this->addBlockLog($ev, "place");
-    }
-
-    private function addBlockLog(BlockBreakEvent|BlockPlaceEvent $ev, string $action): void
-    {
-        $log[] = $action;
-        $log[] = $ev->getPlayer()->getXuid();
-
-        $pos = $ev->getBlock()->getPosition();
-        $log[] = $pos->getFloorX();
-        $log[] = $pos->getFloorY();
-        $log[] = $pos->getFloorZ();
-        $log[] = $pos->getWorld()->getFolderName();
-
-        $log[] = $ev->getItem()->getName();
-        $log[] = $ev->getBlock()->getName();
-        $this->repo->addBlockLog($log);
+        $this->repo->addBlockLog($ev, "place");
     }
 }
--- src/ree_jp/mysql_logger/MysqlLoggerPlugin.php
+++ src/ree_jp/mysql_logger/MysqlLoggerPlugin.php
@@ -3,13 +3,18 @@
 namespace ree_jp\mysql_logger;
 
 use pocketmine\plugin\PluginBase;
+use ree_jp\mysql_logger\sql\BulkRepo;
+use ree_jp\mysql_logger\sql\FileRepo;
 
 class MysqlLoggerPlugin extends PluginBase
 {
 
     public function onEnable(): void
     {
-        $repo = new Repository($this, $this->getConfig());
+        $repo = match ($this->getConfig()->get("type")) {
+            "bulk" => new BulkRepo($this, $this->getConfig()),
+            "file" => new FileRepo($this, $this->getConfig())
+        };
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($repo), $this);
     }
 
